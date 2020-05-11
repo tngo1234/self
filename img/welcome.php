a7cba8d8b3432d90dfb04ae5e3d5b7bd1dcfe4ff
tngo1234 <65199348+tngo1234@users.noreply.github.com>
2020-05-12 07:14:45 +0800

    1
    1

---
+++ img/welcome.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="icon" href="images/favicon.ico" />
+<title>~ Welcome iDonate~</title>
+<link type="text/css" rel="stylesheet" href="away.css" />
+</head>
+
+<body>
+<h1>~ Welcome iDonate~</h1>
+    <div class="bContainer">
+        <a href="index.php">
+        <div class="logo" >ENTER</div>
+        <div class="box1"></div>
+        <div class="box2"></div>
+        <div class="box3"></div>
+        <div class="box4"></div>
+        <div class="box5"></div>      
+        </a>
+    </div>
+
+</body>
+</html>
